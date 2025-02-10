4ae4a8719835560da1162221ec3aee7b377f7860
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2025-02-10 16:49:53 +0000

    more video recording

--- videos/promises_and_fetch/video_1.js
+++ videos/promises_and_fetch/video_1.js
@@ -1,12 +1,3 @@
-// [
-// VIDEO HERE - GO OVER:
-// - Execution order 
-// - Showing pending promise 
-// - Showing res.json()
-// - Show anti-pattern - for instance, try to push an element to an array outside the function, and show that it's not possible to do that. 
-// - Show that you can't return anything outside of a promise chain
-// ]
-
 /* Video aims: 
     - Reinforce knowledge of asynchronous behaviour in JavaScript
     - Learn about the async/await syntax for promises
@@ -15,8 +6,20 @@
  */
 
 
+const dataArray = [];
     
-const fetchData = async () => {
+const fetchData = async () => { //an async function contains long running tasks 
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu"); //sends an HTTP request to the relevant server based on the url you pass as its argument.
+    const data = await response.json();
+    dataArray.push(data);
     return response; 
 }
+
+// const syncFunction = () => { 
+//     return "This is a sync function"
+// }
+
+fetchData();
+console.log(dataArray);
+// console.log(syncFunction());
+
